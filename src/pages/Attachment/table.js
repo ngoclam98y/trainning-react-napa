b4faf9cc21ee144851d7296c93a8d91713865466
ngoc lam <ngoclamdk43@gmail.com>
2022-04-22 16:37:42 +0700

    fix delete file

--- src/pages/Attachment/table.js
+++ src/pages/Attachment/table.js
@@ -1,9 +1,12 @@
 import { DeleteOutlined } from "@ant-design/icons";
-import { Table, Typography } from "antd";
+import { Popconfirm, Table, Typography } from "antd";
+import { useState } from "react";
 import { default as Docx, default as Mp4 } from "../../assets/docx.png";
 import Jpg from "../../assets/jpg.png";
 import Zip from "../../assets/zip.png";
 const { Text } = Typography;
+
+const text = "Are you sure to delete this file?";
 
 export const fileIcon = (fileName) => {
   if (fileName.includes(".docx")) {
@@ -20,47 +23,9 @@ export const fileIcon = (fileName) => {
   }
 };
 
-const columns = [
-  {
-    title: "ファイル名",
-    dataIndex: "fileName",
-    key: "fileName",
-    width: "50%",
-    render: (text) => (
-      <span>
-        {fileIcon(text)}
-        {text}
-      </span>
-    ),
-  },
-  {
-    title: "作成日",
-    dataIndex: "createdAt",
-    key: "createdAt",
-  },
-  {
-    title: "サイズ",
-    dataIndex: "size",
-    key: "size",
-  },
-  {
-    title: "更新日",
-    key: "updatedAt",
-    dataIndex: "updatedAt",
-  },
-  {
-    title: "",
-    key: "action",
-    dataIndex: "action",
-    render: () => (
-      <Text type="danger">
-        <DeleteOutlined />
-      </Text>
-    ),
-  },
-];
 const data = [
   {
+    id: 1,
     key: "1",
     fileName: "発注書.docx",
     createdAt: "2 days ago",
@@ -69,6 +34,7 @@ const data = [
     action: "delete",
   },
   {
+    id: 2,
     key: "7",
     fileName: "イベント１.jpg",
     createdAt: "1 week ago",
@@ -77,6 +43,7 @@ const data = [
     action: "delete",
   },
   {
+    id: 3,
     key: "6",
     fileName: "イベント２.mp4",
     createdAt: "23 hours ago",
@@ -85,6 +52,7 @@ const data = [
     action: "delete",
   },
   {
+    id: 4,
     key: "5",
     fileName: "申し込み.zip",
     createdAt: "2 minutes ago",
@@ -93,6 +61,7 @@ const data = [
     action: "delete",
   },
   {
+    id: 5,
     key: "4",
     fileName: "案内書.docx",
     createdAt: "2 days ago",
@@ -101,6 +70,7 @@ const data = [
     action: "delete",
   },
   {
+    id: 6,
     key: "3",
     fileName: "紹介.mp4",
     createdAt: "23 hours ago",
@@ -109,6 +79,7 @@ const data = [
     action: "delete",
   },
   {
+    id: 7,
     key: "2",
     fileName: "紹介.jpg",
     createdAt: "1 week ago",
@@ -119,5 +90,57 @@ const data = [
 ];
 
 export default function () {
-  return <Table columns={columns} dataSource={data} pagination={false} />;
+  const [dataTable, setDataTable] = useState([...data]);
+
+  const columns = [
+    {
+      title: "ファイル名",
+      dataIndex: "fileName",
+      key: "fileName",
+      width: "50%",
+      render: (text) => (
+        <span>
+          {fileIcon(text)}
+          {text}
+        </span>
+      ),
+    },
+    {
+      title: "作成日",
+      dataIndex: "createdAt",
+      key: "createdAt",
+    },
+    {
+      title: "サイズ",
+      dataIndex: "size",
+      key: "size",
+    },
+    {
+      title: "更新日",
+      key: "updatedAt",
+      dataIndex: "updatedAt",
+    },
+    {
+      title: "",
+      key: "action",
+      dataIndex: "action",
+      render: (_, options) => (
+        <Text style={{ cursor: "pointer" }} type="danger">
+          <Popconfirm
+            placement="top"
+            title={text}
+            onConfirm={() => {
+              const newArr = dataTable.filter((x) => x.id !== options.id);
+              setDataTable(newArr);
+            }}
+            okText="Yes"
+            cancelText="No"
+          >
+            <DeleteOutlined />
+          </Popconfirm>
+        </Text>
+      ),
+    },
+  ];
+  return <Table columns={columns} dataSource={dataTable} pagination={false} />;
 }
