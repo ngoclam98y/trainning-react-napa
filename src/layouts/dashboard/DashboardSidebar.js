c0d9a3fe167d925e2235b92c77fbe53f07a8dfb5
ngoc lam <ngoclamdk43@gmail.com>
2022-04-26 08:54:41 +0700

    fix comma

--- src/layouts/dashboard/DashboardSidebar.js
+++ src/layouts/dashboard/DashboardSidebar.js
@@ -1,11 +1,11 @@
 import { Layout, Menu } from "antd";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { SIDEBAR, SIDEBAR_BOTTOM } from "../../_mocks_/sidebar";
 const { Sider } = Layout;
 
 export default function DashboardSidebar({ collapsed, toggle }) {
-  const location = useLocation();
+  // const location = useLocation();
   const navigation = useNavigate();
   const handleLogout = () => {
     window.localStorage.removeItem("isLogin");
@@ -14,11 +14,11 @@ export default function DashboardSidebar({ collapsed, toggle }) {
 
   const renderMenuItem = (sidebars) => {
     if (!sidebars && !sidebars.length) return null;
-    const isSelected = (
-      sidebars.findIndex((x) => x.link === location.pathname) + 1
-    ).toString();
+    // const isSelected = (
+    //   sidebars.findIndex((x) => x.link === location.pathname) + 1
+    // ).toString();
     return (
-      <Menu theme="white" mode="inline" defaultSelectedKeys={[isSelected]}>
+      <Menu theme="white" mode="inline" defaultSelectedKeys={["1"]}>
         {sidebars.map((v) => (
           <Menu.Item key={v.id} icon={v.icon}>
             {v.link === "/logout" ? (
