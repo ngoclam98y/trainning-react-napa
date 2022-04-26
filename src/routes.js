34c9934952188b287874ba16f13f561dc8b9cd27
ngoc lam <ngoclamdk43@gmail.com>
2022-04-26 09:29:26 +0700

    fix

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Navigate, useRoutes } from "react-router-dom";
-// import Attachment from "./pages/Attachment";
-// import PostSettings from "./pages/PostSettings";
-// import Settings from "./pages/Settings";
+import Attachment from "./pages/Attachment";
+import PostSettings from "./pages/PostSettings";
+import Settings from "./pages/Settings";
 
-// const Login = React.lazy(() => import("./pages/Login"));
-// const LayoutDashBoard = React.lazy(() => import("./layouts/dashboard"));
-// const UserPage = React.lazy(() => import("./pages/User"));
+const Login = React.lazy(() => import("./pages/Login"));
+const LayoutDashBoard = React.lazy(() => import("./layouts/dashboard"));
+const UserPage = React.lazy(() => import("./pages/User"));
 
 function ProtectedRoute({ isAllowed, redirectPath = "/login", children }) {
   if (!isAllowed) {
@@ -17,71 +17,71 @@ function ProtectedRoute({ isAllowed, redirectPath = "/login", children }) {
 }
 
 export default function Router() {
-  // const isLogin = JSON.parse(window.localStorage.getItem("isLogin")) || false;
+  const isLogin = JSON.parse(window.localStorage.getItem("isLogin")) || false;
   return useRoutes([
     {
       path: "/dashboard",
-      element: () => <h1>aaaaaaaaaa</h1>,
+      element: <LayoutDashBoard />,
       children: [
-        // {
-        //   path: "user",
-        //   element: (
-        //     <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
-        //       <UserPage />
-        //     </ProtectedRoute>
-        //   ),
-        // },
-        // {
-        //   path: "file",
-        //   element: (
-        //     <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
-        //       <Attachment />
-        //     </ProtectedRoute>
-        //   ),
-        // },
-        // {
-        //   path: "setting",
-        //   element: (
-        //     <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
-        //       <Settings />
-        //     </ProtectedRoute>
-        //   ),
-        // },
-        // {
-        //   path: "update-setting",
-        //   element: (
-        //     <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
-        //       <PostSettings />
-        //     </ProtectedRoute>
-        //   ),
-        // },
-        // {
-        //   path: "setting-script",
-        //   element: (
-        //     <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
-        //       <h1>setting script</h1>
-        //     </ProtectedRoute>
-        //   ),
-        // },
-        // {
-        //   path: "charts",
-        //   element: (
-        //     <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
-        //       <h1>charts</h1>
-        //     </ProtectedRoute>
-        //   ),
-        // },
+        {
+          path: "user",
+          element: (
+            <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
+              <UserPage />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "file",
+          element: (
+            <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
+              <Attachment />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "setting",
+          element: (
+            <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
+              <Settings />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "update-setting",
+          element: (
+            <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
+              <PostSettings />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "setting-script",
+          element: (
+            <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
+              <h1>setting script</h1>
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "charts",
+          element: (
+            <ProtectedRoute redirectPath="/login" isAllowed={isLogin}>
+              <h1>charts</h1>
+            </ProtectedRoute>
+          ),
+        },
       ],
     },
-    // {
-    //   path: "/login",
-    //   element: <Login />,
-    // },
-    // {
-    //   path: "/",
-    //   element: <LayoutDashBoard />,
-    //   children: [{ path: "/", element: <Navigate to="/dashboard/user" /> }],
-    // },
-    // { path: "*", element: <h1>404</h1> },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/",
+      element: <LayoutDashBoard />,
+      children: [{ path: "/", element: <Navigate to="/dashboard/user" /> }],
+    },
+    { path: "*", element: <h1>404</h1> },
   ]);
 }
